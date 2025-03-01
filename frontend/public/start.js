6fb8e2763c59daff995d86dd4161fd8376977065
CosminOstafie <cosminoa@yahoo.com>
2025-03-01 22:52:10 +0100

    Updated error in fetch

--- frontend/public/start.js
+++ frontend/public/start.js
@@ -4,7 +4,7 @@ fetch('http://localhost:3000/api/stores')
     const ul = document.getElementById('store-list');
     stores.forEach(store =>{
         const li = document.createElement("li")
-        li.innerHTML = `${store.name} - Website: <a href=${store.url || 'Not Available'}> - District: ${store.district || "Not Available"}`
+        li.innerHTML = `${store.name} - Website: ${store.url || 'Not Available'} - District: ${store.district || "Not Available"}`
         ul.appendChild(li);
     });
 })
